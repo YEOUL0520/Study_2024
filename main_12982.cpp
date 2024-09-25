0b1a44dfb02a4f871716dad7f91fb5c4f898a7b4
hyellm <yhl9779@naver.com>
2024-09-25 20:55:01 +0900

    [2주차] 예산 / LV.1
    [2주차] 예산 / LV.1

---
+++ main_12982.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>  // sort
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int n = 0;
+
+    // 오름차순으로 정렬
+    sort(d.begin(), d.end());
+
+    // 작은 예산부터 부서 지원
+    for (int i = 0; i < d.size(); i++)
+    {
+        if (budget < d[i])
+            break;
+
+        budget -= d[i];
+        n++;  // 지원한 부서 수 ++
+    }
+
+    return n;
+}
