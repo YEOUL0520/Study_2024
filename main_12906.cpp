500b6cb3551bec9f064c4a22d330544f03ed06f3
hyellm <yhl9779@naver.com>
2024-10-04 10:22:16 +0900

    양혜림/4주차
    [4주차] 3진법 뒤집기 / Lv. 1
    https://school.programmers.co.kr/learn/courses/30/lessons/68935
    
    [4주차] 같은 숫자는 싫어 / Lv. 1
    https://school.programmers.co.kr/learn/courses/30/lessons/12906

---
+++ main_12906.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    int p = 0;
+    int arr_size = arr.size();
+
+    answer.resize(arr_size); // answer의 크기를 arr의 크기로 설정
+    answer[p] = arr[p];  // 첫 번째 값은 무조건 유지
+
+    for (int i = 1; i < arr_size; i++) {
+        if (answer[p] == arr[i])
+            continue;
+        else {
+            p++;
+            answer[p] = arr[i];
+        }
+    }
+
+    answer.resize(p + 1); // answer의 인덱스를 사용하여 크기 설정
+
+    return answer;
+}
